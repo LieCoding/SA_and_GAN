601d997b5e91ffd77192c4950be8331467908864
LieCoding <64515587+LieCoding@users.noreply.github.com>
2021-03-09 19:55:07 +0800

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,5 +1,6 @@
 import logging
 from logging import handlers
+import numpy as np
 import random
 import os
 # 日志操作
@@ -49,3 +50,147 @@ def split_image_data(image_dir,train_rate=0.2,file_postfix='jpg',shuffle=True):
     train_data = image_name_list[:int(train_rate*data_len)]
     test_data = image_name_list[int(train_rate*data_len):]
     return train_data,test_data
+
+# CHW->HWC
+def transepose_image(image,label,predict):
+
+    t_image = []
+    t_label = []
+    t_predict = []
+
+    for i,j,k in zip(label,predict,image):
+        t_label.append(np.transpose(i, (1,2,0)))
+        t_predict.append(np.transpose(j, (1,2,0)))
+        t_image.append(np.transpose(k, (1,2,0)))
+
+    return t_image,t_label,t_predict
+
+#-----------get acc
+
+import numpy as np
+import cv2
+import os
+
+""" 
+混淆矩阵
+P\L     P    N 
+P      TP    FP 
+N      FN    TN 
+"""
+
+# 从文件夹读取文件用
+def flatten_data(data_dir,label_list,pre_list):
+    label_data_list = []
+    pre_data_list = []
+    for i in range(len(label_list)):
+        # 读取灰度图
+        label_data = cv2.imread(os.path.join(data_dir,label_list[i]),0).astype(np.uint8)
+        pre_data = cv2.imread(os.path.join(data_dir,pre_list[i]),0).astype(np.uint8)
+
+        # 二值化，大于1的等于1
+        _,label_data=cv2.threshold(label_data,1,1,cv2.THRESH_TRUNC)
+        _,pre_data=cv2.threshold(pre_data,1,1,cv2.THRESH_TRUNC)
+
+        label_data_list.append(label_data)
+        pre_data_list.append(pre_data)
+    label_data_list = np.array(label_data_list)
+    pre_data_list =  np.array(pre_data_list)
+    label_data_list = label_data_list.flatten()
+    pre_data_list = pre_data_list.flatten()
+    # print(label_data_list.shape)
+    return label_data_list,pre_data_list
+
+# 训练过程使用
+def process_data(label_list,pre_list):
+    '''
+     处理数据
+    :param label_list: label np格式列表
+    :param pre_list:  预测图片 np格式列表
+    :return: flatten的数据
+    '''
+    label_data_list = []
+    pre_data_list = []
+    for i in range(len(label_list)):
+        # 二值化，大于1的等于1
+        _,label_data=cv2.threshold(label_list[i].astype(np.uint8),1,1,cv2.THRESH_TRUNC)
+        _,pre_data=cv2.threshold(pre_list[i].astype(np.uint8),1,1,cv2.THRESH_TRUNC)
+
+        label_data_list.append(label_data)
+        pre_data_list.append(pre_data)
+    label_data_list = np.array(label_data_list)
+    pre_data_list =  np.array(pre_data_list)
+    label_data_list = label_data_list.flatten()
+    pre_data_list = pre_data_list.flatten()
+    # print(label_data_list.shape)
+    return label_data_list,pre_data_list
+
+
+def ConfusionMatrix(numClass, imgPredict, Label):
+    #  返回混淆矩阵
+    mask = (Label >= 0) & (Label < numClass)
+    label = numClass * Label[mask] + imgPredict[mask]
+    count = np.bincount(label, minlength = numClass**2)
+    confusionMatrix = count.reshape(numClass, numClass)
+    return confusionMatrix
+
+
+def OverallAccuracy(confusionMatrix):
+    #  返回所有类的整体像素精度OA
+    # acc = (TP + TN) / (TP + TN + FP + TN)
+    OA = np.diag(confusionMatrix).sum() / confusionMatrix.sum()
+    return OA
+
+def Precision(confusionMatrix):
+    #  返回所有类别的精确率precision
+    precision = np.diag(confusionMatrix) / confusionMatrix.sum(axis = 1)
+    return precision
+
+def Recall(confusionMatrix):
+    #  返回所有类别的召回率recall
+    recall = np.diag(confusionMatrix) / confusionMatrix.sum(axis = 0)
+    return recall
+
+def F1Score(confusionMatrix):
+    precision = np.diag(confusionMatrix) / confusionMatrix.sum(axis = 1)
+    recall = np.diag(confusionMatrix) / confusionMatrix.sum(axis = 0)
+    f1score = 2 * precision * recall / (precision + recall)
+    return f1score
+def IntersectionOverUnion(confusionMatrix):
+    #  返回交并比IoU
+    intersection = np.diag(confusionMatrix)
+    union = np.sum(confusionMatrix, axis = 1) + np.sum(confusionMatrix, axis = 0) - np.diag(confusionMatrix)
+    IoU = intersection / union
+    return IoU
+
+def MeanIntersectionOverUnion(confusionMatrix):
+    #  返回平均交并比mIoU
+    intersection = np.diag(confusionMatrix)
+    union = np.sum(confusionMatrix, axis = 1) + np.sum(confusionMatrix, axis = 0) - np.diag(confusionMatrix)
+    IoU = intersection / union
+    mIoU = np.nanmean(IoU)
+    return mIoU
+
+def Frequency_Weighted_Intersection_over_Union(confusionMatrix):
+    #  返回频权交并比FWIoU
+    freq = np.sum(confusionMatrix, axis=1) / np.sum(confusionMatrix)
+    iu = np.diag(confusionMatrix) / (
+            np.sum(confusionMatrix, axis = 1) +
+            np.sum(confusionMatrix, axis = 0) -
+            np.diag(confusionMatrix))
+    FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
+    return FWIoU
+
+def get_acc(label,predict,classNum=2):
+
+    label_all,predict_all = process_data(label,predict)
+    #  计算混淆矩阵及各精度参数
+    confusionMatrix = ConfusionMatrix(classNum, predict_all, label_all)
+    precision = Precision(confusionMatrix)
+    recall = Recall(confusionMatrix)
+    # OA = OverallAccuracy(confusionMatrix)
+    IoU = IntersectionOverUnion(confusionMatrix)
+    # FWIOU = Frequency_Weighted_Intersection_over_Union(confusionMatrix)
+    # mIOU = MeanIntersectionOverUnion(confusionMatrix)
+    f1ccore = F1Score(confusionMatrix)
+    return precision[1],recall[1],IoU[1],f1ccore[1]
+
